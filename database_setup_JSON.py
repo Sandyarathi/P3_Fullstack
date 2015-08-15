6deeaa9b703d7e82fdfd13355005ff944fc14241
yongkie <whytwokie@gmail.com>
2015-08-16 00:21:04 +0200

    P3 todo: separate py file

--- database_setup_JSON.py
+++ database_setup_JSON.py
@@ -7,6 +7,13 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 class Category(Base):
     __tablename__ = 'category'
@@ -30,11 +37,14 @@ class Event(Base):
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
+    image = Column(String(250))
     # date = Column(DateTime(250))
     # time = Column(DateTime(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
--- init_events.py
+++ init_events.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup_JSON import Category, Base, Event
+from database_setup_JSON import Category, Base, Event, User
 
 engine = create_engine('sqlite:///eventlist.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -17,6 +17,15 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
+# Create initial user
+User1 = User(name="Why", email="whytwokie@gmail.com",
+             picture='')
+session.add(User1)
+session.commit()
+User2 = User(name="Yongkie", email="yongkie@wiyogo.com",
+             picture='')
+session.add(User2)
+session.commit()
 
 #--------Culture---------
 category_culture = Category(name="Culture")
@@ -25,14 +34,16 @@ session.add(category_culture)
 session.commit()
 
 event1 = Event(name="Piccaso 3.0", description="Modern arts",
-                     price="$7.50", category=category_culture)
+					image="https://upload.wikimedia.org/wikipedia/en/7/76/Pablo_Picasso%2C_1918%2C_Pierrot%2C_oil_on_canvas%2C_92.7_x_73_cm%2C_Museum_of_Modern_Art.jpg",
+                     price="$7.50", category=category_culture, user_id = User1.id)
 
 session.add(event1)
 session.commit()
 
 
-event2 = Event(name="Asia-Europe Traditional Dance", description="A new combination of dancing styles",
-                     price="$22.99", category=category_culture)
+event2 = Event(name="Asia Europe Meetup", description="Meetup for all asian and europe people",
+                     price="$22.99", image="http://cdn.eso.org/images/newsfeature/ann15033a.jpg",
+                     category=category_culture, user_id = User2.id)
 
 session.add(event1)
 session.commit()
@@ -45,13 +56,15 @@ session.add(category_nightlife)
 session.commit()
 
 
-party1 = Event(name="Party party", description="",
-                     price="$7.99", category=category_nightlife)
+party1 = Event(name="Party party", description="Our party",
+					image="http://us.123rf.com/450wm/chagin/chagin1310/chagin131000169/23221929-young-people-having-fun-dancing-at-party.jpg",
+                     price="$7.99", category=category_nightlife, user_id = User2.id)
 session.add(party1)
 session.commit()
 
-party2 = Event(name="90ies Party ", description="",
-                     price="$9.99", category=category_nightlife)
+party2 = Event(name="90ies Party ", description="Party with 90ies music",
+				image="http://diginights.com/uploads/images/event/2013/05/08/2013-05-08-90er-party-garage-saarbruecken/flyer_image-default-1.jpg",
+                 price="$9.99", category=category_nightlife, user_id = User1.id)
 
 session.add(party2)
 session.commit()
@@ -62,14 +75,16 @@ category_sport = Category(name="Sport")
 session.add(category_sport)
 session.commit()
 
-sport1 = Event(name="Marathon ", description="",
-                     price="$15.00", category=category_sport)
+sport1 = Event(name="Marathon ", description="Sport marathon 2015",
+				image ="http://www.sportonline-foto.de/thumbs/KBM14/ip/KBM14CF011000_0032.jpg",
+                 price="$15.00", category=category_sport)
 
 session.add(sport1)
 session.commit()
 
-sport2 = Event(name="LA Lakers vs Chicago Bulls", description="",
-                     price="$45.00", category=category_sport)
+sport2 = Event(name="Chicago Bulls vs LA Lakers", description="Basketball event",
+				image="http://cdn.fansided.com/wp-content/blogs.dir/24/files/2011/12/Chicago-Bulls-vs-LA-Lakers.jpg",
+                     price="$45.00", category=category_sport, user_id = User1.id)
 
 session.add(sport2)
 session.commit()
@@ -81,7 +96,15 @@ session.add(category_culinary)
 session.commit()
 cookevent1 = Event(
     name="Peking Duck Cooking", description=" the diners by the cook",
-     price="$8", category=category_culinary)
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Peking_Duck_1.jpg/800px-Peking_Duck_1.jpg",
+     price="$8", category=category_culinary, user_id = User1.id)
+
+session.add(cookevent1)
+session.commit()
+cookevent1 = Event(
+    name="Organic Burger", description="Meet and Cook organic burger",
+    image="http://thumbs.dreamstime.com/x/organic-grilled-black-bean-burger-tomato-lettuce-35167678.jpg",
+     price="$10", category=category_culinary, user_id = User1.id)
 
 session.add(cookevent1)
 session.commit()
